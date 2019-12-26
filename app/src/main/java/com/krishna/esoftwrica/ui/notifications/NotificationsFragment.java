c01856bc78a75a3a9a4e8f271b94452e9887d46d
krishnadevkota <devkotakrishna71@gmail.com>
2019-12-26 22:21:16 +0545

    Final eSoftwarica added

---
+++ app/src/main/java/com/krishna/esoftwrica/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,38 @@
+package com.krishna.esoftwrica.ui.notifications;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.krishna.esoftwrica.R;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+    private WebView webview;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+
+        webview=root.findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://softwarica.edu.np/");
+        return root;
+
+    }
+
+}
